ac54ad0f80ee4f5707d0efa9f70d51d7e5ddcec7
loktoutou2016 <loktoutou2016@gmail.com>
2020-12-28 10:04:16 +0800

    1

---
+++ src/i18n/index.js
@@ -0,0 +1,7 @@
+import enUS from './en-us'
+import zhHK from './zh-hk'
+
+export default {
+  'en-us': enUS,
+  'zh-hk': zhHK
+}
---
+++ src/i18n/zh-hk/index.js
@@ -0,0 +1,8 @@
+// This is just an example,
+// so you can safely delete all default props below
+
+export default {
+  lang: '中文',
+  failed: '操作失敗',
+  success: '操作成功'
+}
